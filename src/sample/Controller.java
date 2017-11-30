b8fb7dff0f549555803d0607dbce6649a3b88077
BrandoRubio <33293044+BrandoRubio@users.noreply.github.com>
2017-11-30 13:08:07 -0600

    Documentación

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -50,50 +50,72 @@ public class Controller implements Initializable{
         //Valores para Nodo2
         nodo2.setNodoIzquierdo(new Nodo(12));
         nodo2.setNodoDerecho(new Nodo(22));
-
+        //Se declcara cuales son las dos ramas de la raiz
         raiz.setNodoIzquierdo(nodo7);
         raiz.setNodoDerecho(nodo9);
+        //Evento para el botón preOrden
         this.preBtn.setOnAction(new EventHandler<ActionEvent>() {
             @Override
             public void handle(ActionEvent event) {
+                //Se agrega la etiqueta a el label
                 labelLbl.setText("Recorrido en preOrden: \n");
+                //Se invoca el método preOrden desde la clase arbol y agrega como parámetro lo que es la raiz y un label en el cual se
+                //Mostrarán el resultado de la lectura en PreOrden
                 arbol.preOrden(raiz,labelLbl);
             }
         });
+        //Evento para el botón inOrden
         this.inBtn.setOnAction(new EventHandler<ActionEvent>() {
             @Override
             public void handle(ActionEvent event) {
                 labelLbl.setText("Recorrido en inOrden: \n");
+                //Se invoca el método inOrden desde la clase arbol y agrega como parámetro lo que es la raiz y un label en el cual se
+                //Mostrarán el resultado de la lectura en InOrden
                 arbol.inOrden(raiz,labelLbl);
             }
         });
+        //Evento para el botón postOrden
         this.postBtn.setOnAction(new EventHandler<ActionEvent>() {
             @Override
             public void handle(ActionEvent event) {
                 labelLbl.setText("Recorrido en postOrden:\n");
+                //Se invoca el método postOrden desde la clase arbol y agrega como parámetro lo que es la raiz y un label en el cual se
+                //Mostrarán el resultado de la lectura en PostOrden
                 arbol.postOrden(raiz,labelLbl);
             }
         });
+        //Evento para el botón contar
         this.contarBtn.setOnAction(new EventHandler<ActionEvent>() {
             @Override
             public void handle(ActionEvent event) {
+                //se invoca el método contar de la clase arbol y se asignan los parámetros de raiz y un label
                 arbol.contar(raiz,labelLbl);
             }
         });
+        //Evento del botón MaxMin
         this.maxminBtn.setOnAction(new EventHandler<ActionEvent>() {
             @Override
             public void handle(ActionEvent event) {
+                //método maxmin de la clase arbol y se le agrega como parámetro la raiz del árbol y un label
                 arbol.maxmin(raiz,labelLbl);
             }
         });
+        //Evento para el botón buscar
         this.buscarBtn.setOnAction(new EventHandler<ActionEvent>() {
             @Override
             public void handle(ActionEvent event) {
+                //Se inicia un try-catch que sirve para saber si la prueba falla
+                //y de esto sabremos porque falla despues se sale y no hace nada, despues
+                //si se vuelve a intentar ya procede sin fallas
                     try {
+                        //se invoca el método buscar de la clase arbol y se asigna como parámetro la raiz del árbol
+                        //un label  y lo que contenga el cuadro de texto BuscarTxt convertido a enteros
                         arbol.buscar(raiz, labelLbl, Integer.parseInt(buscarTxt.getText()));
                     } catch (Exception e) {
+                        //Si falló el try-catch retornará un mensaje
                         labelLbl.setText("Ingresa números");
                     }
+                    //se limpia el cuadro de texto.
                     buscarTxt.clear();
             }
         });
